154c9d4bd46f46ee7d9e28103f015ca460676828
temka <temuulenb26@gmail.com>
2020-12-05 23:59:03 +0800

    eelj solih shoog alga bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалдаг 1 хувьсагч хэрэг болно. Нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 //Тоглогчдыг оноог цуглуулах 1 хувьсагч
 var scores = [0, 0];
 // Түр оноог цуглуулах хувьсагч
@@ -13,9 +13,22 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDOM = document.querySelector(".dice");
 diceDOM.style.display = "none";
+
+//Шоог шидэх эвент listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
   diceNumber = Math.floor(Math.random() * 6) + 1;
   //   alert("shoog shidlee : " + diceNumber);
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + diceNumber + ".png";
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDOM.style.display = "none";
+  }
 });
