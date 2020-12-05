450a2dc9d2e71cb87db604c6bc398d5cf297cf15
temka <temuulenb26@gmail.com>
2020-12-05 23:23:02 +0800

    shoog haydag bolgoloos

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ var scores = [0, 0];
 // Түр оноог цуглуулах хувьсагч
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувсагч хэрэгтэй. 1-6 гэсэн утгыг хадгалах хувьсагч
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = "";
 // <div class="player-score" id="score-0">43</div>
-window.document.querySelector("#score-0").textContent = "0";
-window.document.querySelector("#score-1").textContent = "0";
-
-document.querySelector("#current-0").innerHTML = "0";
-document.querySelector("#current-0").innerHTML = "0";
-console.log("Шоо :" + dice);
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDOM = document.querySelector(".dice");
+diceDOM.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  diceNumber = Math.floor(Math.random() * 6) + 1;
+  //   alert("shoog shidlee : " + diceNumber);
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + diceNumber + ".png";
+});
