fda05c29db7f14b616151d928a854797cf013df8
Temka <temuulenb26@gmail.com>
2020-12-07 01:00:04 +0800

    hiij duusgalaa

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var isNewGame;
 var activePlayer;
 var scores;
 var roundScore;
@@ -6,6 +7,7 @@ var diceDOM = document.querySelector(".dice");
 initGame();
 
 function initGame() {
+  isNewGame = true;
   //Тоглогчийн ээлжийг хадгалдаг 1 хувьсагч хэрэг болно. Нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
   activePlayer = 0;
   //Тоглогчдыг оноог цуглуулах 1 хувьсагч
@@ -37,34 +39,45 @@ function initGame() {
 
 //Шоог шидэх event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  diceNumber = Math.floor(Math.random() * 6) + 1;
-  //   alert("shoog shidlee : " + diceNumber);
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + diceNumber + ".png";
-  if (diceNumber !== 1) {
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  if (isNewGame) {
+    diceNumber = Math.floor(Math.random() * 6) + 1;
+    //   alert("shoog shidlee : " + diceNumber);
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + diceNumber + ".png";
+    if (diceNumber !== 1) {
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      switchToNextPlayer();
+    }
   } else {
-    switchToNextPlayer();
+    alert("Тоглоом дууссан байна шинээр эхлэнэ үү !!!");
   }
 });
 
 //Оноогоо цуглуулах event listener
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  scores[activePlayer] = scores[activePlayer] + roundScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 20) {
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document.querySelector(".player-1-panel").classList.remove("active");
+  if (isNewGame) {
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      isNewGame = false;
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document.querySelector(".player-1-panel").classList.remove("active");
+    } else {
+      switchToNextPlayer();
+    }
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
   } else {
-    switchToNextPlayer();
+    alert("Тоглоом дууссан байна шинээр эхлэнэ үү !!!");
   }
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
 });
 //Шинэ тоглоом эхлүүлэх товчны event listener
 document.querySelector(".btn-new").addEventListener("click", initGame);
